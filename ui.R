0bc8e462ac0fa29de16c1922be5cbab049f8e3f8
martinjhnhadley <martinjohnhadley@gmail.com>
2017-12-13 12:32:52 +0000

    Added shinyjs and CSS for pulsating "loading data" thing

--- ui.R
+++ ui.R
@@ -1,14 +1,37 @@
 library(leaflet)
+library("shinyjs")
+
+appCSS <- "
+#loading-content {
+position: absolute;
+background: #FFFFFF;
+opacity: 0.9;
+z-index: 100;
+left: 0;
+right: 0;
+height: 100%;
+text-align: center;
+color: #000000;
+}
+"
 
 bootstrapPage(
+  theme = "animate.min.css",
+  useShinyjs(),
+  inlineCSS(appCSS),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}",
              ".leaflet .legend i{
-               border-radius: 50%;
-               width: 10px;
-               height: 10px;
-               margin-top: 4px;
-              }
-              "),
+             border-radius: 50%;
+             width: 10px;
+             height: 10px;
+             margin-top: 4px;
+             }
+             "),
+  div(id = "loading-content",
+      fluidPage(
+        h2(class = "animated infinite pulse", "Loading data...")
+        # HTML("<img src=images/cruk-logo.png width='50%'></img>")
+      )),
   leafletOutput("map", width = "100%", height = "100%"
                 ),
   absolutePanel(
